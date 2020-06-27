7d3d80a45c576fa7854bfe8ae2d5a669e08bf4dc
Chris McLain <christopher@homerunss.com>
2020-06-27 17:32:14 -0500

    Implemented business category search option

---
+++ Giveback/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace VisaHackathon2020.Giveback
+{
+    public static class Extensions
+    {
+        public static string ToTitleCase(this string str)
+        {
+            var textInfo = new CultureInfo("en-US", false).TextInfo;
+            return textInfo.ToTitleCase(str.ToLower());
+        }
+    }
+}
---
+++ Giveback/MerchantCategoryService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualBasic.FileIO;
+
+namespace VisaHackathon2020.Giveback
+{
+    public static class MerchantCategoryService
+    {
+        private static Collection<MerchantCategory> _categories;
+
+        public static Collection<MerchantCategory> Categories => GetCategories();
+
+        private static Collection<MerchantCategory> GetCategories()
+        {
+            if (_categories != null)
+            {
+                return _categories;
+            }
+
+            using var parser = new TextFieldParser(@"merchant_category_codes.csv")
+            {
+                TextFieldType = FieldType.Delimited,
+                Delimiters = new []{","}
+            };
+                
+            _categories = new Collection<MerchantCategory>();
+
+            while (!parser.EndOfData)
+            {
+                var fields = parser.ReadFields();
+                    
+                _categories.Add(new MerchantCategory
+                {
+                    Id = int.Parse(fields[0]),
+                    Name = fields[1]
+                });
+            }
+
+            return _categories;
+        }
+    }
+
+    public class MerchantCategory
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
